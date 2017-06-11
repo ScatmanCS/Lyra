9bccf62a8ed3c2fc0c546d3e0285e468cbabf02a
Scatman <scatmanthememer@gmail.com>
2017-06-11 17:29:14 +0100

    Implementing key features, crates next.

--- src/me/scatmancs/Lyra/Lyra.java
+++ src/me/scatmancs/Lyra/Lyra.java
@@ -3,6 +3,10 @@ package me.scatmancs.Lyra;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import me.scatmancs.Lyra.commands.Command_ores;
+import me.scatmancs.Lyra.commands.Command_ping;
+import me.scatmancs.Lyra.commands.Command_report;
+import me.scatmancs.Lyra.commands.Command_setslots;
+import me.scatmancs.Lyra.listeners.DonatorJoinListener;
 import me.scatmancs.Lyra.listeners.ElevatorListener;
 
 public class Lyra extends JavaPlugin {
@@ -12,6 +16,7 @@ public class Lyra extends JavaPlugin {
 		getLogger().info("Lyra has been enabled!");
 		registerListeners();
 		registerCommands();
+		// loadConfig();
 	}
 	
 	public void onDisable()
@@ -22,10 +27,20 @@ public class Lyra extends JavaPlugin {
 	public void registerListeners()
 	{
 		getServer().getPluginManager().registerEvents(new ElevatorListener(), this);
+		getServer().getPluginManager().registerEvents(new DonatorJoinListener(), this);
+	}
+	
+	public void loadConfig()
+	{
+		getConfig().options().copyDefaults();
+		saveConfig();
 	}
 	
 	public void registerCommands()
 	{
 		getCommand("ores").setExecutor(new Command_ores());
+		getCommand("ping").setExecutor(new Command_ping());
+		getCommand("setslots").setExecutor(new Command_setslots());
+		getCommand("report").setExecutor(new Command_report());
 	}
 }
